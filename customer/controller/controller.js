4ed75916e2676d17ccffbf257a5d161f785f6a4f
hatem <hatemazaiez1@gmail.com>
2022-10-05 22:08:24 +0100

    cc

--- customer/controller/controller.js
+++ customer/controller/controller.js
@@ -263,7 +263,7 @@ exports.LostPassword = async(req,res)=>{
     try{
         let codeDoc = await data.asyncFindOne({code:code});
         let codeDoc1 = await data.asyncFindOne({});
-        console.log(codeDoc);
+        console.log(codeDoc1);
         if(codeDoc != null){
             console.log("Invalid code check your email !");
             return res.status(400).json("Invalid code check your email !");
